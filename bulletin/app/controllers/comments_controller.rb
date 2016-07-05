20b872dafefe9d01eaff7b1d4252b1494f15487f
Anh Truong <anh.truong.07@cnu.edu>
2016-07-05 17:48:54 -0400

    working on sum crud

--- bulletin/app/controllers/comments_controller.rb
+++ bulletin/app/controllers/comments_controller.rb
@@ -7,5 +7,10 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
+  def show
+    @board = Board.find(params[:id])
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:id])
+  end
 
 end
